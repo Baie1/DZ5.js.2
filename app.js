1b9fb8c27bcc883cd9ba5c9f748dd5259fe1054f
Baie1 <115315059+Baie1@users.noreply.github.com>
2022-11-26 18:06:09 +0600

    DZ

--- app.js
+++ app.js
@@ -1,42 +1,38 @@
 const som = document.querySelector("#som")
 const usd = document.querySelector("#usd")
 const euro = document.querySelector("#euro")
-
-
-const convert = (elem, target,target2) => { 
-    elem.addEventListener("input", () => {
-    const request = new XMLHttpRequest()
-    request.open("GET", "data.json")
-    request.setRequestHeader("Content-type", "application/json")
-    request.send()
-    request.addEventListener("load", () =>{
-        const response = JSON.parse(request.response)
-       if (elem === som ) {
-        target.value = (elem.value / response.usd) .toFixed(2);
-        target2.value = (elem.value / response.euro) .toFixed(2);
-       }
-    else if (elem === usd) {
-        target.value = (elem.value * response.usd ) .toFixed(2);
-        target2.value = (elem.value * response.usd / response.euro) .toFixed(2);
-    } else if (elem === euro) {
-        target.value = (elem.value * response.euro ) .toFixed(2);
-        target2.value = (elem.value * response.euro / response.usd) .toFixed(2);
-    }
-
-
-
-
-        elem.value === "" && (target.value = "") 
-        elem.value === "" && (target2.value = "") 
-        }
-        )
-        }
-    )
+const convert = (elem, elem2, elem3) => {
+elem.addEventListener("input", () => {
+// const request = new XMLHttpRequest()
+// request.open("GET", "data.json")
+// request.setRequestHeader("Content-type", "application/json")
+// request.send()
+// request.addEventListener("load", () => {
+// const response = JSON.parse(request.response)
+const response = fetch("data.json")
+.then((response) => response.json())
+.then(response => {
+switch (elem) {
+case som:
+elem2.value = (elem.value / response.usd).toFixed(2)
+elem3.value = (elem.value / response.euro).toFixed(2)
+break;
+case usd:
+elem2.value = (elem.value * response.usd).toFixed(2)
+elem3.value = ((elem.value * response.usd) / response.euro).toFixed(2)
+break;
+case euro:
+elem2.value = (elem.value * response.euro).toFixed(2)
+elem3.value = ((elem.value * response.euro) / response.usd).toFixed(2)
+break;
 }
-
-convert(som,usd,euro);
-convert(usd,som,euro);
-convert(euro,som,usd);
-   
-
-    
+if (elem.value === "") {
+elem2.value = ""
+elem3.value = ""
+}
+})
+})
+}
+convert(som, usd, euro)
+convert(usd, som, euro)
+convert(euro, som, usd)
